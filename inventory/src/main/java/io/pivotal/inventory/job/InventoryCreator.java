579e40b16c38fc24d98a3ae129a76269f4a5d8e8
aniruthmp <aniruth.parthasarathy@gmail.com>
2019-02-23 13:39:45 -0500

    Add Stock object and data populator

--- inventory/src/main/java/io/pivotal/inventory/job/InventoryCreator.java
+++ inventory/src/main/java/io/pivotal/inventory/job/InventoryCreator.java
@@ -2,7 +2,9 @@ package io.pivotal.inventory.job;
 
 import com.github.javafaker.Faker;
 import io.pivotal.inventory.domain.Catalog;
+import io.pivotal.inventory.domain.Stock;
 import io.pivotal.inventory.repository.CatalogRepository;
+import io.pivotal.inventory.repository.StockRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,15 +16,20 @@ public class InventoryCreator implements CommandLineRunner {
 
     @Autowired
     private CatalogRepository catalogRepository;
+    @Autowired
+    private StockRepository stockRepository;
 
-    private final int MAX_RECORDS = 100;
+    private final int MAX_RECORDS = 10;
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("Inventory Data getting created..");
+        log.info("Stock Data getting created..");
         Faker faker = new Faker();
+
         catalogRepository.deleteAll();
         log.info("Cleared old catalog");
+        stockRepository.deleteAll();
+        log.info("Cleared old stock");
 
         for (int i = 0; i < MAX_RECORDS; i++) {
             Catalog catalog = new Catalog();
@@ -31,6 +38,11 @@ public class InventoryCreator implements CommandLineRunner {
             catalog.setProductName(faker.medical().medicineName());
             catalog.setDescription(faker.medical().symptoms());
             catalogRepository.save(catalog);
+
+            Stock stock = new Stock();
+            stock.setProductCode(catalog.getProductCode());
+            stock.setStockRemaining((short) faker.number().numberBetween(0, 100));
+            stockRepository.save(stock);
         }
         log.info("Created new catalog");
     }
